503f6c97dc0b47e6c74f3143ee06fea53138e025
Neo Zhuo <neowiz@icloud.com>
2018-03-01 18:51:43 +0800

    MOD: Add hack scripts

---
+++ hack/check.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+Packages="$(go list ./... | grep -v -E 'vendor|backbone' | xargs echo)"
+
+echo "Checking format..."
+go list ./... | grep -v vendor | sed -e s=github.com/oif/gokit/=./= | xargs -n 1 gofmt -l
+
+echo "Checking govet..."
+go vet $Packages
+
+set -e
+echo "Testing..."
+
+function runtest {
+  bash -c "umask 0; PATH=$GOROOT/bin:$(pwd)/bin:$PATH go test $@"
+}
+
+for t in ${Packages}; do
+  runtest "${t}"
+done
